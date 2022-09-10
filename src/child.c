d9dc6aaa4e04a2f3e91932d8beb402d95ec975de
Nicholas Choo <nchoo@u91z06s02.42kl.edu.my>
2022-09-10 20:50:20 +0800

    minor changes, checkout main

--- src/child.c
+++ src/child.c
@@ -6,7 +6,7 @@
 /*   By: nchoo <nchoo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 15:35:08 by nchoo             #+#    #+#             */
-/*   Updated: 2022/09/10 20:22:55 by nchoo            ###   ########.fr       */
+/*   Updated: 2022/09/10 20:35:46 by nchoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,26 @@
 
 void dup_stdin(int i, char **av, int **fd)
 {
+	// close(fd[i][1]);
 	if (i == 0)
 		dup2(open(av[1], O_RDONLY), STDIN);
 	else
 	{
 		dup2(fd[i][0], STDIN);
 	}
+	close(fd[i][0]);
 }
 
 void dup_stdout(int i, int ac, char **av, int **fd)
 {
+	// close(fd[i][0]);
 	if (i == ac - 4) 
-		dup2(open(av[ac - 1], O_WRONLY | O_CREAT), STDOUT);
+		dup2(open(av[ac - 1], O_WRONLY), STDOUT);
 	else
 	{
 		dup2(fd[i][1], STDOUT);
 	}
+	close(fd[i][1]);
 }
 
 void make_child(t_data *data, char **av, int **fd, char **env)
@@ -39,7 +43,7 @@ void make_child(t_data *data, char **av, int **fd, char **env)
 	pid = fork();
 	if (pid == 0)
 	{
-		ft_printf("pid:%d\n", pid);
+		ft_printf("i:%d, pid:%d\n",data->i, pid);
 		dup_stdin(data->i, av, fd);
 		dup_stdout(data->i, data->ac, av, fd);
 		run_process(data->i + 2, av, env);
